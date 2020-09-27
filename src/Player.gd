f920c8dae4bc8772d8a5bd440d5359a00a4a94e6
JoelDeFriesBSU <jtdefries@bsu.edu>
2020-09-27 03:18:09 -0400

    Implement gravity and jumping for player

--- src/Player.gd
+++ src/Player.gd
@@ -1,31 +1,48 @@
 extends KinematicBody2D
 
 
-onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
+onready var gravity = ProjectSettings.get("physics/2d/default_gravity") * 0.2
 onready var animated_sprite = $AnimatedSprite
-onready var camera = $Camera2D
 
+
+var velocity = Vector2()
+var jumping = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var camera = $Camera2D
 
 
 func _physics_process(delta):
+	# Side-to-side movement
 	var direction = get_direction()
 	var linear_velocity = direction * 300
-	var vertical_velocity = Vector2.ZERO
 	var _ignored = move_and_slide(linear_velocity)
 	
 	
-	vertical_velocity.y += gravity * 100
+	# Gravity using Vector2 velocity
+	velocity.y += gravity + delta
+	velocity = move_and_slide(velocity, Vector2(0,-1))
 	
+	
+	# Jumping
+	if Input.is_action_just_pressed("jump") and !jumping:
+		velocity.y = -525
+		jumping = true
+	
+	if is_on_floor():
+		jumping = false
+	
+	
+	# Sprite flipping based on direction
 	if direction.x != 0:
 		animated_sprite.scale.x = 1 if direction.x > 0 else -1
 		animated_sprite.play("walk")
-
 	if direction.x == 0:
 		animated_sprite.play("idle")
+	
+
+
 
 
 func get_direction():
