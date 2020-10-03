25e4d8809770c1858e6e12925bbd14764cf9e766
JoelDeFriesBSU <jtdefries@bsu.edu>
2020-10-03 15:29:54 -0400

    Remove commented out process_delta in Gem.gd

--- src/Gem.gd
+++ src/Gem.gd
@@ -9,13 +9,6 @@ func _ready():
 	$CollisionPolygon2D.disabled = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-
-
 func _on_Gem_body_entered(body):
 	emit_signal("gem_collected")
 	queue_free()
