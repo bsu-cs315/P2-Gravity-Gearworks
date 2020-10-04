19174c5a713c57be568339f9b9e426ba603f7d5b
JoelDeFriesBSU <jtdefries@bsu.edu>
2020-10-04 19:02:11 -0400

    Add functionality to exit door (send player to EndScreen scene)

--- src/Level.gd
+++ src/Level.gd
@@ -1,29 +1,26 @@
 extends Node2D
 
 
+var exit_door_entered = 0
 var score = 0
 
 
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("jump") and exit_door_entered == 1:
+		var _ignored = get_tree().change_scene("res://src/EndScreen.tscn")
 
 
 func _on_ExitDoor_body_entered(body):
 	$ExitDoor/ExitLabel.show()
-	if Input.is_action_just_pressed("jump"):
-		print("Level ending...")
-		# TODO: Move scene to game over scene.
+	exit_door_entered = 1
 
 
 func _on_ExitDoor_body_exited(body):
 	$ExitDoor/ExitLabel.hide()
-
-
+	exit_door_entered = 0
 
 
 func _on_Gem_gem_collected():
