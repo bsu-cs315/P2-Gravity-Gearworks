dd6888d707a70d69b2174f4656f88bc5c7935be9
JoelDeFriesBSU <jtdefries@bsu.edu>
2020-10-04 20:37:02 -0400

    Implement and randomize gem 'deployment' upon entering level

--- src/Gem.gd
+++ src/Gem.gd
@@ -3,10 +3,15 @@ extends Area2D
 
 signal gem_collected
 
+var rng = RandomNumberGenerator.new()
+var delay_time = rng.randf_range(0.01,0.99)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CollisionPolygon2D.disabled = false
+	$AnimationPlayer/RandomDelay.wait_time = delay_time
+	$AnimationPlayer/RandomDelay.start()
+	$AnimationPlayer.play_backwards("collected")
 
 
 func _on_Gem_body_entered(body):
